f6c6328b0d7bbdabf3f44fdc28822324e8c1499a
Nathan Sheyman <nathansheyman@icloud.com>
2023-11-04 16:21:11 -0700

    changed

--- backend.js
+++ backend.js
@@ -1,1 +1,9 @@
-console.log("H");
+function addBook() {
+    
+    alert("Book added!"); 
+}
+
+var button = document.getElementById("addBookButton");
+
+
+button.addEventListener("click", addBook);
